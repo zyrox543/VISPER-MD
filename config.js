24f496517b3a99da6736f9542c9b146de63415f2
vispermdoffical <vispermd@gmail.com>
2025-05-21 13:14:30 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
+
+};
+
